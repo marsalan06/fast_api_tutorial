54549b3fce6215b780ce3f5faca0bc8adeecbba1
marsalan06 <arsalan.9798@gmail.com>
2023-03-12 13:10:34 +0500

    initial setup

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return {'data':{'name':"Arsalan"}}
+
+
+
+@app.get('/about')
+def about():
+    return {'data':'about page'}
+
+
