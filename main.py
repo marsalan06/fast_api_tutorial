76ad0fd7b6acb67fe67e80cf0f8b987629e647b2
marsalan06 <arsalan.9798@gmail.com>
2023-03-12 14:03:32 +0500

    get methods with query params added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -12,4 +13,21 @@ def index():
 def about():
     return {'data':'about page'}
 
+
+@app.get('/blog/complete')
+def complete():
+    return {'data':'blog route without type issue'}
+
+@app.get('/blog/{id}') #dynamic url
+def index(id: int): #fix the type by adding id: int 
+    return {'data':'blog list '+ str(id)}
+
+@app.get('/query')
+def query_method(number,published: Optional[bool]=True): #so now number is required but published is optional and has a default value
+    #http://127.0.0.1:8000/query?number=3&published=true
+    if published:
+        return {"returning all data":f"{number}"}
+    #http://127.0.0.1:8000/query?number=3&published=false
+    else:
+        return {"returning some data":f"{number}"}
 
